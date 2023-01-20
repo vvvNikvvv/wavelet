c95148f8fbafb462a4b45ad89c4393b492c65f52
vvvNikvvv <nvirrey@ucsd.edu>
2023-01-19 18:10:10 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -8,17 +8,17 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Nik's Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
-            return String.format("Number incremented!");
+            return String.format("Nik's Number incremented!");
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("count")) {
                     num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                    return String.format("Nik's Number increased by %s! It's now %d", parameters[1], num);
                 }
             }
             return "404 Not Found!";
